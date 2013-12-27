2db375a1b951193d7639dd78739d68a3d2ee67e6
EmilGeorgiev <emogeorgiev88@gmail.com>
2013-12-27 16:09:02 +0200

    adding task 1 and 2 by Testing with mocks

---
+++ TreningTDD/test/com.clouway.roof/TestRoof.java
@@ -0,0 +1,15 @@
+package com.clouway.roof;
+
+import org.junit.Test;
+
+/**
+ * Created by clouway on 12/21/13.
+ */
+public class TestRoof {
+
+  @Test
+  public void test() throws Exception {
+
+
+  }
+}

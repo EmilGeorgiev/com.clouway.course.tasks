c42303b7b7fc072182938a8cd198258197643c85
EmilGeorgiev <emogeorgiev88@gmail.com>
2014-01-16 09:25:52 +0200

    Finish task 2.2 from Threads after check.Adding "setThread(Thread thread)", and removed "static field boolean".

--- 9.Threads/src/limitcounterthreads/CounterThread.java
+++ 9.Threads/src/limitcounterthreads/CounterThread.java
@@ -3,11 +3,10 @@ package limitcounterthreads;
 /**
  * Created by clouway on 1/15/14.
  */
-public class CounterThread extends Thread{
+public class CounterThread extends Thread {
   private final int limit;
-
-  static boolean finishCount = false;
-  private int count;
+  private int count = 0;
+  private CounterThread thread;
 
   public CounterThread(int limit) {
 
@@ -16,13 +15,18 @@ public class CounterThread extends Thread{
 
   @Override
   public void run() {
-    while (!finishCount) {
-      if (count == limit) {
-        finishCount = true;
+      while (count != limit) {
+        if (isInterrupted()) {
+          System.out.println(getName() + " interrupted.");
+          break;
+        }
+        count++;
+        System.out.println(getName() + " count " + count);
       }
-      count++;
-      System.out.println(getName() + " count to: " + count);
-    }
-    System.out.println(getName() + " finished.");
+      thread.interrupt();
+  }
+
+  public void setThread(CounterThread thread) {
+    this.thread = thread;
   }
 }
--- 9.Threads/src/limitcounterthreads/CounterThreadDemo.java
+++ 9.Threads/src/limitcounterthreads/CounterThreadDemo.java
@@ -6,7 +6,11 @@ package limitcounterthreads;
 public class CounterThreadDemo {
   public static void main(String[] args) {
     CounterThread counterTo100 = new CounterThread(100);
+    counterTo100.setPriority(1);
     CounterThread counterTo200 = new CounterThread(200);
+    counterTo100.setThread(counterTo200);
+    counterTo200.setThread(counterTo100);
+    counterTo200.setPriority(10);
     counterTo100.start();
     counterTo200.start();
   }
